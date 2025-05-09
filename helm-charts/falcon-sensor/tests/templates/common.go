bd62a065a743bba77cdffb79502e82f1093f9cc2
Greg Pontejos <greg.pontejos@crowdstrike.comC>
2025-05-09 13:53:49 -0500

    initial commit

---
+++ helm-charts/falcon-sensor/tests/templates/common.go
@@ -0,0 +1,5 @@
+package template_test
+
+const (
+	cid = "1234567890ABCDEF1234567890ABCDEF-12"
+)
---
+++ helm-charts/falcon-sensor/tests/templates/daemonset_test.go
@@ -0,0 +1,33 @@
+package template_test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func TestDaemonsetTemplateRender(t *testing.T) {
+	releaseName := "test-release"
+	helmChartPath := "../../../falcon-sensor"
+	options := &helm.Options{
+		SetValues: map[string]string{"falcon.cid": cid},
+	}
+
+	output := helm.RenderTemplate(
+		t, options, helmChartPath, releaseName,
+		[]string{"templates/configmap.yaml"})
+
+	var configMap corev1.ConfigMap
+	helm.UnmarshalK8SYaml(t, output, &configMap)
+	t.Logf("configMap: %v", configMap)
+	// expectedContainerImage := "nginx:1.15.8"
+	// podContainers := configMap.Spec.Containers
+	// if podContainers[0].Image != expectedContainerImage {
+	// 	t.Fatalf(
+	// 		"Rendered container image (%s) is not expected (%s)",
+	// 		podContainers[0].Image,
+	// 		expectedContainerImage,
+	// 	)
+	// }
+}
